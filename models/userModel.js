3b9b6e6e6e3830e69084b7311ab1095e5da1f9bd
Akande Lateef <akandelateef0@gmail.com>
2024-12-12 23:58:06 +0100

    add: user model

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const Schema = mongoose.Schema
+
+userSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// run this function before creating the user account
+userSchema.pre(
+    'save',
+    async function (next) {
+        const hash = await bcrypt.hash(this.password, 10);
+
+        this.password = hash;
+        next();
+    }
+);
+
+
+const userModel = mongoose.model('user', userSchema)
+module.exports = userModel
