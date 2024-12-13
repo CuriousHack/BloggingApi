d68410b59fe26ffaaf2647ccbfd74cdad0319a73
Akande Lateef <akandelateef0@gmail.com>
2024-12-13 01:56:51 +0100

    add: input validation added for registration

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const authController = require('../controllers/authControllers')
+const validateRequest = require('../middlewares/validateRequest')
+const { registerSchema } = require('../validations/authValidation')
 
 const router = express.Router()
 
-router.post("/register", authController.register);
+router.post("/register", validateRequest(registerSchema), authController.register);
 
 module.exports = router
---
+++ validations/authValidation.js
@@ -0,0 +1,33 @@
+const Joi = require('joi')
+
+const registerSchema = Joi.object({
+    firstname: Joi.string()
+            .min(2).max(30)
+            .required()
+            .messages({
+      "any.required": "Firstname is required",
+    }),
+    lastname: Joi.string()
+            .min(2).max(30)
+            .required()
+            .messages({
+      "any.required": "Lastname is required",
+    }),
+    email: Joi.string()
+            .email()
+            .required()
+            .messages({
+      "string.email": "Invalid email format",
+      "any.required": "Email is required",
+    }),
+
+    password: Joi.string()
+                .min(6)
+                .required()
+                .messages({
+                    "string.min": "Password must be at least 6 characters",
+                    "any.required": "Password is required",
+    }),
+  });
+
+  module.exports = { registerSchema };
