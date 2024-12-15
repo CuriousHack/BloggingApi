588792ee83b786fc19eac0664918bb9252758f91
Akande Lateef <akandelateef0@gmail.com>
2024-12-15 03:05:25 +0100

    add: jwt token verifier added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const passport = require('passport')
-const userRoutes = require('./routes/userRoute')
+const authRoutes = require('./routes/authRoute')
+const postRoutes = require('./routes/postRoute')
 const connectDb = require('./utils/db')
+const userRoutes = require('./routes/userRoute')
+validateToken = require('./middlewares/validateToken')
 require('dotenv').config()
 
 
@@ -18,7 +21,9 @@ connectDb()
 app.get('/api', (req, res) => {
     res.status(200).json({message: "Welcome to BloggingAPI"})
 })
-app.use('/api/auth', userRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/user', validateToken, userRoutes)
 
 
 app.listen(PORT, () => {
